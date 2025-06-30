dccf2a5044f01553769c436278732a7dcc875cdd
nani-mex <drnoey782@gmail.com>
2025-06-30 08:38:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -23,16 +23,6 @@ buttons.forEach(button =>{
             display.value += value;
         }
 
-        if (value === 'AC') {
-            display.value = '';
-        }
-        else if (value === '=') {
-            try {
-                display.value = eval(display.value);
-            } catch (error) {
-                display.value = 'I love U';
-            }
-        
         }
         else if (value === 'x2') {
             if (display.value) {
@@ -63,11 +53,6 @@ buttons.forEach(button =>{
             if (display.value) {
                 display.value = Math.sqrt(parseFloat(display.value));
             }
-        
-        
-        
-        
-    
 
     });
 
