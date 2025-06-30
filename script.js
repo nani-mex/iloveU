9e9adcc6b0a7b3b6c79122910058c9202d7d5fbc
nani-mex <drnoey782@gmail.com>
2025-06-30 07:33:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,65 @@ buttons.forEach(button =>{
             display.value += value;
         }
 
+        if (value === 'AC') {
+            display.value = '';
+        }
+        else if (value === '=') {
+            try {
+                display.value = eval(display.value);
+            } catch (error) {
+                display.value = 'I love U';
+            }
+        }
+        else if (value === '+/-') {
+            if (display.value) {
+                display.value = parseFloat(display.value) * -1;
+            }
+        }
+        else if (value === 'x²') {
+            if (display.value) {
+                display.value = Math.pow(parseFloat(display.value), 2);
+            }
+        }
+        else if (value === 'x³') {
+            if (display.value) {
+                display.value = Math.pow(parseFloat(display.value), 3);
+            }
+        }
+        else if (value === '(' || value === ')') {
+            display.value += value;
+        }
+        else {
+            display.value += value;
+        }
         
+    }
+    else if (value === '√') {
+            if (display.value) {
+                display.value = Math.sqrt(parseFloat(display.value));
+            }
+        
+        }
+        else if (value === 'Emoji') {
+            const emojiMap = {
+                '0': '⓪',
+                '1': '①',
+                '2': '②',
+                '3': '③',
+                '4': '④',
+                '5': '⑤',
+                '6': '⑥',
+                '7': '⑦',
+                '8': '⑧',
+                '9': '⑨'
+            };
+            
+            display.value = display.value.split('').map(char => emojiMap[char] || char).join('');
+        }
+        else {
+            display.value += value;
+        }
+    
 
     });
 
